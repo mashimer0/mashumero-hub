0cb4a1a414d2db008466a5622bc369ca88cc00d1
mashimer0 <syoueiwatanabe010@gmail.com>
2026-09-17 15:07:40 +0900

    main.lua を作成

---
+++ main.lua
@@ -0,0 +1,8 @@
+-- ==========================================
+-- ましゅめろキック main
+-- ==========================================
+local baseUrl = "https://raw.githubusercontent.com/mashimer0/mashumero-hub/main/"
+
+loadstring(game:HttpGet(baseUrl .. "part1.lua?t=" .. tick()))()
+loadstring(game:HttpGet(baseUrl .. "part2.lua?t=" .. tick()))()
+loadstring(game:HttpGet(baseUrl .. "part3.lua?t=" .. tick()))()
